0547397f23e294e7fc0bf4affbfa16f32247b5ab
Georgii Kalnytskyi <gkalnytskyi@gmail.com>
2013-11-08 21:37:32 +0200

    Added project on String functions practice

---
+++ StringWordsLowerUpper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringWordsLowerUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the sentence, and hit Enter.");
+            string str = Console.ReadLine();
+            string[] strList = str.Trim().
+                                   Split(new char[] { ' ', '\t', '\n' },
+                                         StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < strList.Length; ++i)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(strList[i].ToLower());
+                else
+                    Console.WriteLine(strList[i].ToUpper());
+            }
+            Console.ReadLine();
+        }
+    }
+}
