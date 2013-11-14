d254ed4f1deaf864b44ab87f2095e26af1260890
Georgii Kalnytskyi <gkalnytskyi@gmail.com>
2013-11-14 22:11:04 +0200

    Added CountLetters project

---
+++ CommonFunctions/DataInput.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunctions
+{
+    public class DataInput
+    {
+        public static T EnterValue<T>(string message)
+        {
+            var value = default(T);
+            bool ok = true;
+
+            do
+            {
+                try
+                {
+                    Console.Write(message + " ");
+                    var s = Console.ReadLine();
+                    value = (T)((IConvertible)s).ToType(typeof(T), new CultureInfo("en-en"));
+                    ok = true;
+                }
+                catch
+                {
+                    Console.WriteLine("The entered value is cannot be converted to type: {0}. Try again",typeof(T).ToString());
+                    ok = false;
+                }
+            }
+            while(!ok);
+
+            return value;
+        }
+    }
+}
---
+++ CountLetersInText/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CountLetersInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The app for counting letters in words that begin with capital letter.");
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("CountLetersInText.exe <file path>");
+                return;
+            }
+
+            IEnumerable<string> fileLines;
+            try
+            {
+                int count = LetterCounting(File.ReadLines(args[0]));
+
+                Console.WriteLine("The number of charachters is {0}", count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Path is invalid");
+                Console.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private static int LetterCounting(IEnumerable<string> fileLines)
+        {
+            int count = 0;
+
+            foreach (var line in fileLines)
+            {
+                string[] words = line.Split(new char[] { ' ', '\t', '.', '?', ',', '!', ':' },
+                                            StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (char.IsUpper(word[0]))
+                    {
+                        count += word.Length;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
